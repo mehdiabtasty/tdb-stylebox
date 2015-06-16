04825e439740453d5f99e9f0c263de7671b92d7a
Mehdi <mehdi@abtasty.com>
2015-06-16 17:36:12 +0200

    delete element from tdb australia

--- src/sidebar.php
+++ src/sidebar.php
@@ -37,37 +37,13 @@ else {
 
 		<ul>
 			<li <?= ($current == 'dashboard')?'class="active"':''?>>
-				<a href="index.php" title="dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
-			</li>
-			<li <?= ($current == 'knowledge')?'class="active"':''?>>
-				<a href="index.php?page=knowledge"><i class="fa fa-lg fa-fw fa-cloud"></i> <span class="menu-item-parent">Knowledge</span></a>
-			</li>
-			<li <?= ($current == 'clientStats')?'class="active"':''?>>
-				<a href="index.php?page=clientStats"><i class="fa fa-lg fa-fw fa-bar-chart"></i> <span class="menu-item-parent">Client stats</span></a>
-			</li>
-			<li <?= ($current == 'pendingTests')?'class="active"':''?>>
-				<a href="index.php?page=pendingTests"><i class="fa fa-lg fa-fw fa-play-circle"></i> <span class="menu-item-parent">Pending tests</span></a>
-			</li>
-			<li <?= ($current == 'translations')?'class="active"':''?>>
-				<a href="index.php?page=translations"><i class="fa fa-lg fa-fw fa-comments-o"></i> <span class="menu-item-parent">Translations</span></a>
-			</li>
-			<li <?= ($current == 'validatePlugins')?'class="active"':''?>>
-				<a href="index.php?page=validatePlugins"><i class="fa fa-lg fa-fw fa-plug"></i> <span class="menu-item-parent">Validate plugins</span></a>
-			</li>
-			<li <?= ($current == 'translatePlugins')?'class="active"':''?>>
-				<a href="index.php?page=translatePlugins"><i class="fa fa-lg fa-fw fa-comment-o"></i> <span class="menu-item-parent">Translate plugins</span></a>
-			</li>
-			<li <?= ($current == 'lastTestsRanking')?'class="active"':''?>>
-				<a href="index.php?page=lastTestsRanking"><i class="fa fa-lg fa-fw fa-signal"></i> <span class="menu-item-parent">Last 24h tests ranking</span></a>
-			</li>
-			<li <?= ($current == 'webinar')?'class="active"':''?>>
-				<a href="index.php?page=webinar"><i class="fa fa-lg fa-fw fa-calendar"></i> <span class="menu-item-parent">Webinar management</span></a>
+				<a href="index.php" title="dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span><span class="badge pull-right inbox-badge">12</span></a>
 			</li>
 			<li <?= ($current == 'tables')?'class="active"':''?>>
 				<a href="index.php?component=tables"><i class="fa fa-lg fa-fw fa-table"></i> <span class="menu-item-parent">UI : Tables</span></a>
 			</li>
 			<li <?= ($current == 'dataTables')?'class="active"':''?>>
-				<a href="index.php?page=dataTables"><i class="fa fa-lg fa-fw fa-table"></i> <span class="menu-item-parent">UI : DataTables</span><span class="badge pull-right inbox-badge">new</span></a>
+				<a href="index.php?page=dataTables"><i class="fa fa-lg fa-fw fa-table"></i> <span class="menu-item-parent">UI : DataTables</span></a>
 			</li>
 			<li <?= ($current == 'buttons')?'class="active"':''?>>
 				<a href="index.php?component=buttons"><i class="fa fa-lg fa-fw fa-desktop"></i> <span class="menu-item-parent">UI : Buttons</span></a>
